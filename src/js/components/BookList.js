1a85f8161c8bf966083784588f91b6d2421bf8ee
Maksymyr <maksymyr@gmail.com>
2017-11-18 00:45:41 +0200

    red

--- src/js/components/BookList.js
+++ src/js/components/BookList.js
@@ -139,9 +139,9 @@ export default class BookList extends React.Component {
                 if(this.state.books.length>21){
                 for(var i=1; i<Math.floor(this.state.books.length/21)+1; i++){
                     if(a[i]=='undefined'){
-                        a[i]=<Link to={`/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
+                        a[i]=<Link to={`/snails/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
                     }else{
-                        a[i]=<Link to={`/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
+                        a[i]=<Link to={`/snails/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
                     }
                 }
             }else{
@@ -150,7 +150,7 @@ export default class BookList extends React.Component {
                         if(i==1){
                             a[i]=<Link to="/" onClick={this.scrolling} key={i}><p className='pageP'>1</p></Link>
                         }else{
-                            a[i]=<Link to={`/pages${i}`} onClick={this.scrolling} key={i}><p className='pageP'>{i} </p></Link>
+                            a[i]=<Link to={`/snails/pages${i}`} onClick={this.scrolling} key={i}><p className='pageP'>{i} </p></Link>
                         }
                     }
                 }
@@ -167,18 +167,18 @@ export default class BookList extends React.Component {
                 if(this.state.books.length>7){
                 for(var i=1; i<Math.floor(this.state.books.length/7)+1; i++){
                     if(a[i]=='undefined'){
-                        a[i]=<Link to={`/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
+                        a[i]=<Link to={`/snails/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
                     }else{
-                        a[i]=<Link to={`/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
+                        a[i]=<Link to={`/snails/pages${i+1}`} onClick={this.scrolling} key={i}><p className='pageP'>{i+1} </p></Link>
                     }
                 }
             }else{
                 for(var i=1; i<Math.floor(this.props.l/7)+2; i++){
                     if(+this.props.pages!=i){
                         if(i==1){
-                            a[i]=<Link to="/" onClick={this.scrolling} key={i}><p className='pageP'>1</p></Link>
+                            a[i]=<Link to="/snails/" onClick={this.scrolling} key={i}><p className='pageP'>1</p></Link>
                         }else{
-                            a[i]=<Link to={`/pages${i}`} onClick={this.scrolling} key={i}><p className='pageP'>{i} </p></Link>
+                            a[i]=<Link to={`/snails/pages${i}`} onClick={this.scrolling} key={i}><p className='pageP'>{i} </p></Link>
                         }
                     }
                 }
