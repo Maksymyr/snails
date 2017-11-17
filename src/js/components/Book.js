8181bf89a5848271d07b03e949bbbc7275dcd92e
Maksymyr <maksymyr@gmail.com>
2017-11-18 00:41:47 +0200

    edit

--- src/js/components/Book.js
+++ src/js/components/Book.js
@@ -58,15 +58,15 @@ export default class Book extends React.Component {
 
                      <div className="future" ref="futures" style={this.props.item.futured?{backgroundImage: 'url('+ require("../../icon/heart-fill.png")+')'}:{backgroundImage: 'url('+ require("../../icon/heart-empty1.png")+')'} } onClick={this.handleClick}></div> 
 
-                    <Link onClick={this.scrolling} to={"/page"+this.props.item.code}><img src={this.props.item.img? this.props.item.img: require('../../image/no-image.png')}/></Link>
+                    <Link onClick={this.scrolling} to={"/snails/page"+this.props.item.code}><img src={this.props.item.img? this.props.item.img: require('../../image/no-image.png')}/></Link>
                     <div className="book_stars">
                         
                         {this.star(this.props.item.rating)}
 
                     </div>
                     <div className="clear"></div>
-                    <p className="book_text"><Link onClick={this.scrolling} className="book_link" to={"/page"+this.props.item.code}>{this.props.item.name}</Link></p>
-                    <p className="book_author"><Link onClick={this.scrolling} className="book_link" to={"/author/"+this.props.item.author}>{this.props.item.author}</Link></p>
+                    <p className="book_text"><Link onClick={this.scrolling} className="book_link" to={"/snails/page"+this.props.item.code}>{this.props.item.name}</Link></p>
+                    <p className="book_author"><Link onClick={this.scrolling} className="book_link" to={"/snails/author/"+this.props.item.author}>{this.props.item.author}</Link></p>
                     {this.props.item.seria? <p className="book_seria">{"Серия: " +this.props.item.seria}</p>: null}
                     
                     
